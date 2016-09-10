BUG: corrected sign error in Nickel, added constants

friendlyfit/modules/engines/nickel.py
@@ -1,5 +1,8 @@
+from math import isnan
+
 import numpy as np
 
+from ...constants import LOG2
 from ..module import Module
 
 CLASS_NAME = 'Nickel'
@@ -9,10 +12,10 @@ class Nickel(Module):
     """Nickel decay engine
     """
 
-    HALF_LIFE = 6.075 * 86400.
-    DECAY_ENER = 3.42166689e-6
-    NUM_NI56_SUN = 2.14125561e55
-    DECAY_CONST = NUM_NI56_SUN * DECAY_ENER / HALF_LIFE
+    HALF_LIFE = 6.075  # in days
+    DECAY_ENER = 3.42166689e-6  # in ergs
+    NUM_NI56_SUN = 2.14125561e55  # num ni56 atoms in solar mass of ni56
+    DECAY_CONST = LOG2 * NUM_NI56_SUN * DECAY_ENER / HALF_LIFE
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -23,12 +26,13 @@ class Nickel(Module):
         self.texplosion = kwargs['texplosion']
 
         decay_facs = [np.inf if self.texplosion > x else
-                      ((x - self.texplosion) * 86400. / self.HALF_LIFE)
+                      ((x - self.texplosion) / self.HALF_LIFE)
                       for x in self.times]
         current_mnickel = [self.mnickel * np.power(0.5, x) for x in decay_facs]
+        current_mnickel = [0.0 if isnan(x) else x for x in current_mnickel]
         # print(self.mnickel, min(current_mnickel), max(current_mnickel))
 
-        luminosities = [y * np.power(2.0, x) * np.log(2.0) * self.DECAY_CONST
+        luminosities = [y * np.power(2.0, -x) * self.DECAY_CONST
                         for x, y in zip(decay_facs, current_mnickel)]
         # print(max(luminosities))
         return {'luminosities': luminosities}