7e79fab90d0d477bb4640e48d091f782cf2c6d16
guillochon <guillochon@gmail.com>
2016-09-09 20:50:34 -0400

    ENH: reduce wavelenths burden

--- friendlyfit/modules/seds/blackbody.py
+++ friendlyfit/modules/seds/blackbody.py
@@ -1,4 +1,4 @@
-from math import exp, pi
+from math import exp, pi, sqrt
 
 import numpy as np
 from astropy import constants as c
@@ -16,6 +16,8 @@ class Blackbody(Module):
     FLUX_CONST = (2.0 * c.h / (c.c**2) * pi).value
     X_CONST = (c.h / c.k_B).value
     C_CONST = (c.c / u.Angstrom).value
+    STEF_CONST = (4.0 * pi * c.sigma_sb).value
+    FOUR_PI = 4.0 * pi
     N_PTS = 10
 
     def __init__(self, **kwargs):
@@ -23,10 +25,15 @@ class Blackbody(Module):
         self._wavelengths = []
 
     def process(self, **kwargs):
+        self._luminosities = kwargs['luminosities']
         self._temperature = kwargs['temperature']
+        radii = [sqrt(x / (self.STEF_CONST * self._temperature**4))
+                 for x in self._luminosities]
         a = [self.X_CONST * x / self._temperature for x in self._frequencies]
-        return {'seds': [x**3 * self.FLUX_CONST / (exp(y) - 1.0)
-                for x, y in zip(self._frequencies, a)]}
+        return {'seds': [[
+            self.FOUR_PI * z**2 * x**3 * self.FLUX_CONST / (exp(y) - 1.0)
+            for x, y in zip(self._frequencies, a)
+        ] for z in radii]}
 
     def handle_requests(self, **requests):
         wavelength_ranges = requests.get('wavelengths', [])
@@ -36,5 +43,6 @@ class Blackbody(Module):
             self._wavelengths.extend(
                 list(np.arange(rng[0], rng[1], (rng[1] - rng[0]) /
                                self.N_PTS)))
-        # self._wavelengths = list(set(self._wavelengths)).sort()
+        self._wavelengths = list(set(self._wavelengths))
+        self._wavelengths.sort()
         self._frequencies = [self.C_CONST / x for x in self._wavelengths]
