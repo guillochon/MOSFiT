34016d88c28093ab29051e192b89c692c91612dd
guillochon <guillochon@gmail.com>
2016-09-12 00:10:20 -0400

    ENH: More `Diffusion` optimization.

--- friendlyfit/modules/transforms/diffusion.py
+++ friendlyfit/modules/transforms/diffusion.py
@@ -15,6 +15,8 @@ class Diffusion(Module):
 
     N_INT_TIMES = 40
     MIN_EXP_ARG = 10
+    DIFF_CONST = 2.0 * M_SUN_CGS / (13.7 * C_CGS * KM_CGS)
+    TRAP_CONST = 3.0 * M_SUN_CGS / (FOUR_PI * KM_CGS**2)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -29,28 +31,27 @@ class Diffusion(Module):
         self._luminosities = kwargs['luminosities']
         self._times_since_exp = [(x - self._t_explosion) * DAY_CGS
                                  for x in self._times]
-        self._tau_diff = np.sqrt(2.0 * self._kappa * self._m_ejecta * M_SUN_CGS
-                                 / (13.7 * C_CGS * (self._v_ejecta * KM_CGS)))
-        self._trap_coeff = (3.0 * self._kappa_gamma * self._m_ejecta *
-                            M_SUN_CGS / (FOUR_PI *
-                                         (self._v_ejecta * KM_CGS)**2))
-        td, A = self._tau_diff, self._trap_coeff
+        self._tau_diff = np.sqrt(self.DIFF_CONST * self._kappa *
+                                 self._m_ejecta / self._v_ejecta)
+        self._trap_coeff = (self.TRAP_CONST * self._kappa_gamma *
+                            self._m_ejecta / (self._v_ejecta**2))
+        td2, A = self._tau_diff**2, self._trap_coeff
 
         new_lum = []
         for te in self._times_since_exp:
             if te <= 0.0:
                 new_lum.append(0.0)
                 continue
-            tb = np.sqrt(max(te**2 - (self.MIN_EXP_ARG * td)**2, 0.0))
+            tb = np.sqrt(max(te**2 - self.MIN_EXP_ARG**2 * td2, 0.0))
             int_times = np.linspace(tb, te, self.N_INT_TIMES)
             int_lums = np.interp(int_times, self._times_since_exp,
                                  self._luminosities)
-            int_arg = ne.evaluate('2.0 * int_lums * int_times / td**2 * '
-                                  'exp((int_times**2 - te**2) / td**2) * '
+            int_arg = ne.evaluate('2.0 * int_lums * int_times / td2 * '
+                                  'exp((int_times**2 - te**2) / td2) * '
                                   '(1.0 - exp(-A / te**2))')
             # int_arg = [
-            #     2.0 * l * t / td**2 *
-            #     np.exp((t**2 - te**2) / td**2) * (1.0 - np.exp(-A / te**2))
+            #     2.0 * l * t / td2 *
+            #     np.exp((t**2 - te**2) / td2) * (1.0 - np.exp(-A / te**2))
             #     for t, l in zip(int_times, int_lums)
             # ]
             int_arg = [0.0 if isnan(x) else x for x in int_arg]
