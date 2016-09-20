82db635ebc9896bbb0bf56f08eaf68e607999e66
guillochon <guillochon@gmail.com>
2016-09-20 18:17:53 -0400

    ENH: getting setup.py ready for release

--- setup.py
+++ setup.py
@@ -2,4 +2,17 @@ from distutils.core import setup
 
 extensions = []
 
-setup()
+setup(
+    name='mosfit',
+    packages=['mosfit'],
+    version='0.1',
+    description=('Package that performs maximum likelihood analysis to fit '
+                 'semi-analytical model predictions to observed '
+                 'astronomical transient data.'),
+    author='James Guillochon',
+    author_email='guillochon@gmail.com',
+    url='https://github.com/guillochon/mosfit',
+    download_url='https://github.com/guillochon/mypackage/tarball/0.1',
+    keywords=['astronomy', 'fitting', 'monte carlo', 'modeling'],
+    classifiers=[]
+)
