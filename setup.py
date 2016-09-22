adb6ece20175d62ce0fb2852800036b5d64824c2
guillochon <guillochon@gmail.com>
2016-09-22 06:35:22 -0700

    MAINT: Eliminated the future

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 import os
 
-from future import __author__, __license__, __version__
 from setuptools import find_packages, setup
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -10,12 +9,12 @@ setup(
     name='mosfit',
     packages=find_packages(),
     include_package_data=True,
-    version=__version__,
+    version=__version__,  # noqa
     description=('Package that performs maximum likelihood analysis to fit '
                  'semi-analytical model predictions to observed '
                  'astronomical transient data.'),
-    license=__license__,
-    author=__author__,
+    license=__license__,  # noqa
+    author=__author__,  # noqa
     author_email='guillochon@gmail.com',
     url='https://github.com/guillochon/mosfit',
     download_url='https://github.com/guillochon/mosfit/tarball/0.1.1',
