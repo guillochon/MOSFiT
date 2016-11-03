881d4ab8482aaea392e3216b4eb05f4b5c325d68
guillochon <guillochon@gmail.com>
2016-11-03 10:44:58 -0400

    MAINT: increasing `Diffusion` constants

--- mosfit/modules/transforms/diffusion.py
+++ mosfit/modules/transforms/diffusion.py
@@ -12,8 +12,8 @@ class Diffusion(Transform):
     """Photon diffusion transform.
     """
 
-    N_INT_TIMES = 200
-    MIN_EXP_ARG = 20
+    N_INT_TIMES = 1000
+    MIN_EXP_ARG = 100
     DIFF_CONST = 2.0 * M_SUN_CGS / (13.7 * C_CGS * KM_CGS)
     TRAP_CONST = 3.0 * M_SUN_CGS / (FOUR_PI * KM_CGS**2)
 
