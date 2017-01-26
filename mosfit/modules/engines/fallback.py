c5bd4c7f1d96456a7928fe6e5964d825d33dd78f
Brenna Mockler <bmockler@ucsc.edu>
2017-01-26 13:21:24 -0800

    cleaned up comments

--- mosfit/modules/engines/fallback.py
+++ mosfit/modules/engines/fallback.py
@@ -25,48 +25,23 @@ class Fallback(Engine):
 		# load dmde info
 
 		#------ DIRECTORY PARAMETERS -> need to change to variable names used in mosfit, then won't have to set any variables here
-		# rundir is the directory holding the original flash data.  
-		# This is only used to get the number of runs (= # of diff beta values) 
-		# & the different beta values from run_info.dat. 
-		#rundir= '/Users/brennamockler/tdefit/dmdts/mpoly_5-3_4-3_1e6/'
 
-		# directory that holds smoothed and rebinned dmdes (NOT original flash dmdes)
-		# It is assumed that there are different files for each beta (such as dmde2.5.dat for beta = 2.5)
+		# It is assumed that there are different files for each beta (such as 2.500.dat for beta = 2.5)
 		# The first row is energy, the second is dmde. This could be changed so that
 		# each beta has a different subdirectory
 
 		# for now just use astrocrash dmdes (converted from astrocrash dmdts)
-		#print (os.path.dirname(__file__)[:-15])
+
 		dmdedir = os.path.dirname(__file__)[:-15]+'models/tde/data/' #'../../models/tde/data/'
 	   
 		#dmdedir = '/Users/brennamockler/Dropbox (Personal)/Research/smooth+rebin/mpoly_5-3_4-3_1e6/gkernel35/'
 
 		#--------- GET SIMULATION BETAS -----------------
 
-		# Following block could be updated to get beta's from file names instead.
-
-		# assumes run_info.dat folder exists with number of betas as first row and then each beta as 
-		# single entry on following rows
-
-		# the dmdes are in '/Users/brennamockler/tdefit/dmdts/mpoly_5-3_(gamma)_1e6/(beta)/dmde.dat'
-		# where gamma = 4-3 or 5-3 and beta is one of the betas in run_info.dat
-	   
-		# following code gets simulation betas if you have 'run_info.dat' file
-		#with open('/Users/brennamockler/tdefit/dmdts/mpoly_5-3_4-3_1e6/'+'run_info.dat') as f:
-    	#   info=f.readlines()
-    	#   nruns=int(info.pop(0)) # number of runs for this gamma value, each run corresponds to different beta
-    	#   sim_beta_str=[]
-    	#   for i in range(nruns):
-    		   # beta values of runs are in run_info.dat after the number of runs,
-    	#	  sim_beta_str.append(info.pop(0).strip()) #.strip() gets rid of trailing '\n' 
-			   
-    	# use sim_beta for figuring out which beta to interpolate between
-		#self._sim_beta=[float(b) for b in sim_beta_str] 
-
 		# hardcode in the simulation betas for gamma = 4-3 for now
 		self._sim_beta = [0.600,0.650,0.700,0.750,0.800,0.850,0.900,1.000,1.100,1.200,1.300,1.400,1.500,1.600,1.700,1.800,1.850,1.900,2.000,2.500,3.000,3.500,4.000]
 
-		#self._str_sim_beta = ['0.600','0.650','0.700','0.750','0.800','0.850','0.900','1.000','1.100','1.200','1.300','1.400','1.500','1.600','1.700','1.800','1.850','1.900','2.000','2.500','3.000','3.500','4.000']
+		
 		#-- CREATE INTERPOLATION FUNCTIONS; FIND SLOPES & YINTERs -------
 
 		# these three lists, in addition to 'sim_beta', are the lists that will hold dmde info to be accessed after init is run
@@ -76,14 +51,12 @@ class Fallback(Engine):
 
 	   # need to pad with extra zeros for dmde files from astrocrash 
 		e_lo, dmde_lo = np.loadtxt(dmdedir+'{:.3f}'.format(self._sim_beta[0])+'.dat') # format requires 3 digits after decimal point
-		#e_lo, dmde_lo = np.loadtxt(dmdedir+str(self._str_sim_beta[0])+'.dat')
 		#e_lo, dmde_lo = np.loadtxt(dmdedir+'dmde'+str(self._sim_beta[0])+'.dat')
 		for i in range(1,len(self._sim_beta)): # bc calculating slope and yintercepts BETWEEN each simulation beta
 			self._energy.append(e_lo) # save to access later in process function
 	 		# dmde.append(dmde_lo) # save to access later in process function --> don't need, can just use interpolations but might not be exact for betas = simulation betas
 			
 			e_hi, dmde_hi= np.loadtxt(dmdedir+'{:.3f}'.format(self._sim_beta[i])+'.dat') #astrocrash format
-			#e_hi, dmde_hi = np.loadtxt(dmdedir+str(self._str_sim_beta[i])+'.dat')
 
 			# smoothed flash file format
 			#e_hi, dmde_hi= np.loadtxt(dmdedir+'dmde'+str(self._sim_beta[i])+'.dat')
@@ -111,11 +84,6 @@ class Fallback(Engine):
 			self._beta_yinter.append((yinterlo+yinterhi)/2.0) # take average of yinterlo and yinterhi to get y intercept used in calculation (note that James just uses yinterlo)
 
 			e_lo, dmde_lo = e_hi, dmde_hi
-
-			#self._mintime1 = 0
-			#self._maxtime1 = 15
-
-		    # Should I deallocate or delete unused variables at the end of __init__ ?
 
 	def process(self, **kwargs):
 	   
@@ -184,15 +152,9 @@ class Fallback(Engine):
 			# only convert dm/de --> dm/dt for mass that is bound to BH (energy < 0)
 			ebound = np.array(self._energy[interp_index_low][self._energy[interp_index_low]<0]) # cuts off part of array with positive e (unbound)
 			dmdebound = np.array(dmde[self._energy[interp_index_low]<0])
-			#print ('beta = '+str(self._beta)+'; bhmass = '+str(kwargs['bhmass']))
-			#print (min(ebound),max(ebound))
-			#print (min(dmdebound),max(dmdebound))
 
 			# calculate de/dt, time and dm/dt arrays
 			dedt = (1.0/3.0)*(-2.0*ebound)**(5.0/2.0)/(2.0*np.pi*G*Mhbase)  # in erg/s
-
-			
-			#print (min(dedt),max(dedt))
 
 			time = (2.0*np.pi*G*Mhbase)*(-2.0*ebound)**(-3.0/2.0)   # in seconds
 			time = time/(24*3600) # time in days
@@ -209,19 +171,6 @@ class Fallback(Engine):
 			dmdt = dmdt*np.sqrt(Mhbase/self._bhmass)
 			time = time*np.sqrt(self._bhmass/Mhbase)
 			
-			# print statemetns for testing
-			#print (min(dmdt),max(dmdt))
-			#print (min(time),max(time))
-			#print (max(time),1.*10**(self._maxtime1))
-			#if max(time)<10.**(self._maxtime1): 
-			#	self._maxtime1 = np.log(max(time))
-			#	print ('log of min of maxtimes is: '+str(self._maxtime1))	
-			
-			#if min(time)>self._mintime1: 	
-			#	self._mintime1 = min(time)
-			#	print ('max of mintimes is: '+str(self._mintime1))		
-    		# interpolate dmdt so that it has values at times = self._time
-			
 			# this assumes t is increasing
 			timeinterp = CubicSpline(time, dmdt)
 			
@@ -230,13 +179,12 @@ class Fallback(Engine):
 
 			# this assumes t is increasing
 			dmdtnew = timeinterp(self._times)
-
-			#{:.3f}'.format(self._sim_beta[0])
-			np.savetxt('test/files/beta'+'{:.3f}'.format(self._beta)+'mbh'+'{:.0f}'.format(self._bhmass)+'.dat',(self._times,dmdtnew),fmt='%1.18e')
 			# this assumes t is decreasing 
 			#dmdtnew = np.flipud(timeinterp(self._times))
 
+			# Can uncomment following line to save files for testing
+			#np.savetxt('test/files/beta'+'{:.3f}'.format(self._beta)+'mbh'+'{:.0f}'.format(self._bhmass)+'.dat',(self._times,dmdtnew),fmt='%1.18e')
+			
 			luminosities = 0.1*dmdtnew*c.c.cgs.value
 
-    		#return {'luminosities': luminosities, 'times': time}
 			return {'kappagamma': kwargs['kappa'], 'luminosities': luminosities}
