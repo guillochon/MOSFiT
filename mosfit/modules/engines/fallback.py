990c825497a5c5a95d1fee70d8868ae6bec7190a
Brenna Mockler <bmockler@ucsc.edu>
2017-02-10 09:18:12 -0800

    makes sure e_lo in bounds of e_hi, adds gamma 5/3 but no gamma interp

--- mosfit/modules/engines/fallback.py
+++ mosfit/modules/engines/fallback.py
@@ -1,3 +1,6 @@
+# has early time extrapolation, makes sure e_lo in bounds of e_hi, and loads in gamma = 5/3 and 4/3 in init
+# although it doesn't do gamma interp yet.
+
 from math import isnan
 
 import astropy.constants as c
@@ -32,57 +35,87 @@ class Fallback(Engine):
 
 		# for now just use astrocrash dmdes (converted from astrocrash dmdts)
 
-		dmdedir = os.path.dirname(__file__)[:-15]+'models/tde/data/' #'../../models/tde/data/'
-	   
-		#dmdedir = '/Users/brennamockler/Dropbox (Personal)/Research/smooth+rebin/mpoly_5-3_4-3_1e6/gkernel35/'
+		self._gammas = ['4-3','5-3']
 
-		#--------- GET SIMULATION BETAS -----------------
+		# dictionaries with gamma's as keys.	
+		self._beta_slope = {self._gammas[0]:[], self._gammas[1]:[]}
+		self._beta_yinter = {self._gammas[0]:[], self._gammas[1]:[]}
+		self._energy = {self._gammas[0]:[], self._gammas[1]:[]}
+		self._sim_beta = {self._gammas[0]:[], self._gammas[1]:[]}
 
-		# hardcode in the simulation betas for gamma = 4-3 for now
-		self._sim_beta = [0.600, 0.650, 0.700, 0.750, 0.800, 0.850, 0.900, 1.000, 1.100,  1.200, 1.300, 1.400, 1.500, 1.600, 1.700, 1.800, 1.850, 1.900, 2.000, 2.500, 3.000, 3.500, 4.000]
+		for g in self._gammas:
+			
+			dmdedir = os.path.dirname(__file__)[:-15] + 'models/tde/data/' + g + '/' #'../../models/tde/data/'
+		   
+			#dmdedir = '/Users/brennamockler/Dropbox (Personal)/Research/smooth+rebin/mpoly_5-3_4-3_1e6/gkernel35/'
 
+			#--------- GET SIMULATION BETAS -----------------
+
+			# hardcode in the simulation betas for gamma = 4-3 for now
+			sim_beta_files = os.listdir(dmdedir)
+			
+			#sim_beta_new = {g:[float(b[:-4]) for b in sim_beta_files]}
+			self._sim_beta[g].extend([float(b[:-4]) for b in sim_beta_files]) #{self._sim_beta.items() + sim_beta_new.items()} #[0.600, 0.650, 0.700, 0.750, 0.800, 0.850, 0.900, 1.000, 1.100,  1.200, 1.300, 1.400, 1.500, 1.600, 1.700, 1.800, 1.850, 1.900, 2.000, 2.500, 3.000, 3.500, 4.000]
+
+			
+			#-- CREATE INTERPOLATION FUNCTIONS; FIND SLOPES & YINTERs -------
+
+			# these three lists, in addition to 'sim_beta', are the lists that will hold dmde info to be accessed after init is run
+			
+			#self._beta_slope = []
+			#self._beta_yinter = []
+			#self._energy = []
+
+		   # need to pad with extra zeros for dmde files from astrocrash 
+			#e_lo, dmde_lo = np.loadtxt(dmdedir+'{:.3f}'.format(self._sim_beta[0])+'.dat') # format requires 3 digits after decimal point
+			e_lo, dmde_lo = np.loadtxt(dmdedir+sim_beta_files[0])
+			#e_lo, dmde_lo = np.loadtxt(dmdedir+'dmde'+str(self._sim_beta[0])+'.dat')
+			for i in range(1,len(self._sim_beta[g])): # bc calculating slope and yintercepts BETWEEN each simulation beta
 		
-		#-- CREATE INTERPOLATION FUNCTIONS; FIND SLOPES & YINTERs -------
+				e_hi, dmde_hi= np.loadtxt(dmdedir+sim_beta_files[i]) #np.loadtxt(dmdedir+'{:.3f}'.format(self._sim_beta[i])+'.dat') #astrocrash format
 
-		# these three lists, in addition to 'sim_beta', are the lists that will hold dmde info to be accessed after init is run
-		self._beta_slope = []
-		self._beta_yinter = []
-		self._energy = []
 
-	   # need to pad with extra zeros for dmde files from astrocrash 
-		e_lo, dmde_lo = np.loadtxt(dmdedir+'{:.3f}'.format(self._sim_beta[0])+'.dat') # format requires 3 digits after decimal point
-		#e_lo, dmde_lo = np.loadtxt(dmdedir+'dmde'+str(self._sim_beta[0])+'.dat')
-		for i in range(1,len(self._sim_beta)): # bc calculating slope and yintercepts BETWEEN each simulation beta
-			self._energy.append(e_lo) # save to access later in process function
-	 		# dmde.append(dmde_lo) # save to access later in process function --> don't need, can just use interpolations but might not be exact for betas = simulation betas
-			
-			e_hi, dmde_hi= np.loadtxt(dmdedir+'{:.3f}'.format(self._sim_beta[i])+'.dat') #astrocrash format
+				# these two if statements needed bc of how interpolation is done
+				# used for ~ 1/3 of data, higher beta have higher spread in e, and this 'mostly' maps out to larger e ranges in sims
+				# e is monotonically increasing, with most negative (most bound) first
+				if min(e_lo) < min(e_hi): 
+					#print ('min(e_lo) < min(e_hi)','gamma:', g, 'beta high:', self._sim_beta[g][i])
+					dmde_lo = dmde_lo[e_lo > min(e_hi)] # match new e_lo array sliced below
+					e_lo = e_lo[e_lo > min(e_hi)] # cut off first e values up to where e_lo within range of e_hi
 
-			# smoothed flash file format
-		 	
-		 	# Interpolate  e array so that we can create same energy steps for lo and hi arrays.
-	 		# since using e_lo array, only need to interpolate hi arrays.
-		 	# (using e_lo array bc it is w/in the energy range of e_hi array)
+				if max(e_lo) > max(e_hi):
+					#print ('max(e_lo) > max(e_hi)','gamma:', g, 'beta high:', self._sim_beta[g][i])
+					dmde_lo = dmde_lo[e_lo < max(e_hi)] # match new e_lo array sliced below
+					e_lo = e_lo[e_lo < max(e_hi)] # cut off last e values up to where e_lo within range of e_hi
 
-			# note that x array for CubicSpline needs to be monotonically increasing
-			funchi = CubicSpline(e_hi, dmde_hi)
-			
-			#funchi = CubicSpline(np.flipud(e_hi), np.flipud(dmde_hi)) 
-		 	
-		 	# get dmde_hi at values of e_lo so I can interpolate in beta
-			dmde_hi_new = funchi(e_lo)
-			#dmde_hi_new = np.flipud(funchi(np.flipud(e_lo)))
+				self._energy[g].append(e_lo) # save to access later in process function
+				# dmde.append(dmde_lo) # save to access later in process function --> don't need, can just use interpolations but might not be exact for betas = simulation betas
 
-			# get slope for linear interpolation (in beta)
-			self._beta_slope.append((dmde_hi_new - dmde_lo)/(self._sim_beta[i]-self._sim_beta[i-1]))
-			
-			# get y intercept for linear interpolation (in beta)
-			yinterlo = dmde_lo - self._beta_slope[-1]*self._sim_beta[i-1]
-			yinterhi = dmde_hi_new - self._beta_slope[-1]*self._sim_beta[i]
+				# smoothed flash file format
+			 	
+			 	# Interpolate  e array so that we can create same energy steps for lo and hi arrays.
+		 		# since using e_lo array, only need to interpolate hi arrays.
+			 	# (using e_lo array bc it is w/in the energy range of e_hi array)
 
-			self._beta_yinter.append((yinterlo+yinterhi)/2.0) # take average of yinterlo and yinterhi to get y intercept used in calculation (note that James just uses yinterlo)
+				# note that x array for CubicSpline needs to be monotonically increasing
+				funchi = CubicSpline(e_hi, dmde_hi)
+				
+				#funchi = CubicSpline(np.flipud(e_hi), np.flipud(dmde_hi)) 
+			 	
+			 	# get dmde_hi at values of e_lo so I can interpolate in beta
+				dmde_hi_new = funchi(e_lo)
+				#dmde_hi_new = np.flipud(funchi(np.flipud(e_lo)))
 
-			e_lo, dmde_lo = e_hi, dmde_hi
+				# get slope for linear interpolation (in beta)
+				self._beta_slope[g].append((dmde_hi_new - dmde_lo)/(self._sim_beta[g][i]-self._sim_beta[g][i-1]))
+				
+				# get y intercept for linear interpolation (in beta)
+				yinterlo = dmde_lo - self._beta_slope[g][-1]*self._sim_beta[g][i-1]
+				yinterhi = dmde_hi_new - self._beta_slope[g][-1]*self._sim_beta[g][i]
+
+				self._beta_yinter[g].append((yinterlo+yinterhi)/2.0) # take average of yinterlo and yinterhi to get y intercept used in calculation (note that James just uses yinterlo)
+
+				e_lo, dmde_lo = e_hi, dmde_hi
 
 	def process(self, **kwargs):
 	   
@@ -95,6 +128,7 @@ class Fallback(Engine):
 		Mhbase = 1.0e6*Msolar # this is the generic size of bh used in astrocrash sim
 		Mstarbase = Msolar
 
+		
 		self._beta = kwargs['beta']
 	   
 		if 'dense_times' in kwargs:
@@ -103,31 +137,31 @@ class Fallback(Engine):
 			self._times = kwargs['rest_times']
 
 	   # Check that beta chosen is within range of simulation betas
-		if self._beta<self._sim_beta[0]:
+		if self._beta<self._sim_beta['4-3'][0]:
 			beta_outside_range=True
 			interp_index_low=0
-			print ('beta below simulation range: '+str(self._sim_beta[0])+'-'+str(self._sim_beta[-1]))
+			print ('beta below simulation range: '+str(self._sim_beta['4-3'][0])+'-'+str(self._sim_beta['4-3'][-1]))
 			print ('choose beta within range')
 			beta_interp=False
 
-		if self._beta>self._sim_beta[-1]:
+		if self._beta>self._sim_beta['4-3'][-1]:
 			beta_outside_range=True
-			interp_index_high=len(self._sim_beta)-1
-			print ('beta above simulation range: '+str(self._sim_beta[0])+'-'+str(self._sim_beta[-1]))
+			interp_index_high=len(self._sim_beta['4-3'])-1
+			print ('beta above simulation range: '+str(self._sim_beta['4-3'][0])+'-'+str(self._sim_beta['4-3'][-1]))
 			print ('choose beta within range')
 			beta_interp=False
 
 	   # find simulation betas to interpolate between
-		for i in range(len(self._sim_beta)):
-			if self._beta==self._sim_beta[i]: # don't need to interpolate, already have dmde and t for this beta
+		for i in range(len(self._sim_beta['4-3'])):
+			if self._beta==self._sim_beta['4-3'][i]: # don't need to interpolate, already have dmde and t for this beta
 				beta_interp=False
-				if i == len(self._beta_slope): # chosen beta value == highest sim beta value
+				if i == len(self._beta_slope['4-3']): # chosen beta value == highest sim beta value
 					interp_index_low = i-1		# interpolations only calculated between values, therefore need to use lower interpolation for this to work
 				else: interp_index_low = i  # so that conversion from dmde --> dmdt works (uses e_lo for conversion)
 
 				#print ('exists simulation beta equal to user beta, no beta interpolation necessary, calculating dmdt...')
 				break
-			if self._beta<self._sim_beta[i]: 
+			if self._beta<self._sim_beta['4-3'][i]: 
 				interp_index_high=i
 				interp_index_low=i-1
 				break
@@ -140,8 +174,15 @@ class Fallback(Engine):
 			#print (len(self._beta_yinter),len(self._beta_slope))
 			#print (interp_index_low, self._sim_beta[interp_index_low])
 			#print (self._beta)
-			dmde = self._beta_yinter[interp_index_low] + self._beta_slope[interp_index_low]*self._beta
+			dmde = self._beta_yinter['4-3'][interp_index_low] + self._beta_slope['4-3'][interp_index_low]*self._beta
 
+			# quick fix for one neg. dmde value:
+			if len(dmde[dmde<0]) > 0:
+				if (dmde[0] < 0) and (len(dmde[dmde<0]) == 1):
+					dmde = dmde[1:]
+					self._energy['4-3'][interp_index_low] = self._energy['4-3'][interp_index_low][1:]
+					print ('negative first value of dmde, beta =',self._beta)
+				else: print ('more than the first value of (beta interpolated) dmde is negative')
 
 			#----------- CONVERT dm/de --> dm/dt --------------
 
@@ -155,22 +196,28 @@ class Fallback(Engine):
 			#   e_lo, dmdenew = np.loadtxt(dmdedir+'dmde'+sim_beta_str[interp_index_low]+'.dat')
 
 			# only convert dm/de --> dm/dt for mass that is bound to BH (energy < 0)
-			ebound = np.array(self._energy[interp_index_low][self._energy[interp_index_low]<0]) # cuts off part of array with positive e (unbound)
-			dmdebound = np.array(dmde[self._energy[interp_index_low]<0])
+			ebound = np.array(self._energy['4-3'][interp_index_low][self._energy['4-3'][interp_index_low]<0]) # cuts off part of array with positive e (unbound)
+			dmdebound = np.array(dmde[self._energy['4-3'][interp_index_low]<0])
 
+
+			if min(dmdebound)<0: 
+
+				print ('beta, negative dmdebound', self._beta, dmdebound[dmdebound<0])
+				
 			# calculate de/dt, time and dm/dt arrays
 			dedt = (1.0/3.0)*(-2.0*ebound)**(5.0/2.0)/(2.0*np.pi*G*Mhbase)  # in erg/s
 
 			time = (2.0*np.pi*G*Mhbase)*(-2.0*ebound)**(-3.0/2.0)   # in seconds
 			time = time/(24*3600) # time in days
-			
+
 			dmdt = dmdebound*dedt 
+			#if len(dmdt[dmdt<0])>0: print ('dmdt',dmdt)
 
 			# ----------- EXTRAPOLATE dm/dt TO EARLY TIMES -------------
 			# new dmdt(t[0]) should == min(old dmdt)
 			# use power law to fit : dmdt = b*t^xi
 
-			# calculate floor dmdt and t to extrapolate down to
+			# calculate floor dmdt and t to extrapolate down to this value for early times
 			dfloor = np.min(dmdt)
 
 			if dmdt[0] >= dfloor*1.01: # not within 1% of floor, extrapolate
@@ -185,16 +232,21 @@ class Fallback(Engine):
 				while (index1 < 8):  # p should not be larger than 0.3
 					p += 0.1
 					index1 = int(ipeak*p)
-					if p == 0.3: break
+					if p >= 0.3: 
+						#print ('enter')
+						break
 
 
 				while (index1-start < 5): # ensure extrapolation will include at least 5 pts 
 					start -=1
 					if start == 0: break
 
+
 				if p*2 < 0.5 : index2 = int(ipeak*(p*2)) # ensure extrap. won't extend more than halfway to peak
 				else: index2 = int(ipeak*0.5)
 
+				#print ('ipeak, p, start, index1, index2', ipeak, p, start, index1, index2)
+				
 				t1 = time[start:index1]
 				d1 = dmdt[start:index1]
 
@@ -202,7 +254,7 @@ class Fallback(Engine):
 				d2 = dmdt[index2 - (index1 - start):index2]
 
 				# exponent for power law fit
-				print (start, index1, index2)
+				#print (start, index1, index2)
 				xi = np.log(d1/d2)/np.log(t1/t2)
 				xiavg = np.mean(xi)
 
@@ -215,12 +267,31 @@ class Fallback(Engine):
 
 				logtfloor = np.log10(dfloor/bavg)/xiavg # log(new start time)
 
-				textp = np.logspace(logtfloor, np.log10(time[start+index1/2]), num = 75) # ending extrapolation here will help make it a smoother transition
+				textp = np.logspace(logtfloor, np.log10(time[start+int(index1/2)]), num = 75) # ending extrapolation here will help make it a smoother transition
 				dextp = bavg*textp**xiavg
 
-				time = np.concatenate((textp,time[start+index1/2 + 1:]))
-				dmdt = np.concatenate((dextp,dmdt[start+index1/2 + 1:]))
+				#print ('min, max textp',min(textp), max(textp))
+				#print ('min, max time',min(time[start+index1/2 + 1:]), max(time[start+index1/2 + 1:]))
+				#print ('min, max dextp',min(dextp), max(dextp))
+				#print ('min, max dmdt',min(dmdt[start+index1/2 + 1:]), max(dmdt[start+index1/2 + 1:]))
+				
 
+				#print (len(time),start+int(index1/2) + 1)
+				#print ('t1,t2',t1, t2)
+				#print ('dmdtfloor',dfloor)
+				#print('logtfloor, tend for textp', logtfloor, np.log10(time[start+int(index1/2)]))
+				#print ('t1,t2',t1, t2)
+				#print ('bavg, xiavg:',bavg, xiavg)
+				#print ('before: textp', textp)
+				#print ('before: dextp', dextp)
+
+				time = np.concatenate((textp,time[start+int(index1/2) + 1:]))
+				dmdt = np.concatenate((dextp,dmdt[start+int(index1/2) + 1:]))
+
+				#print ('after: min, max time',min(time), max(time))
+				#print ('after: min, max dmdt',min(dmdt), max(dmdt))
+				#print ('after: time',time[:len(textp)])
+				#print ('after: dmdt',dmdt[:len(textp)])
 
 			# ----------- SCALE dm/dt TO BH & STAR SIZE --------------
 
