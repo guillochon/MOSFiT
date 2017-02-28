479b5f11cd308c1dbdcfed061e7aae9ab21661b1
guillochon <guillochon@gmail.com>
2017-02-28 11:43:12 -0500

    ENH: changed sphinx docs theme to RTD

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-
+import sphinx_rtd_theme
 
 # -- General configuration ------------------------------------------------
 
@@ -31,9 +31,9 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode']
+extensions = [
+    'sphinx.ext.autodoc', 'sphinx.ext.mathjax', 'sphinx.ext.viewcode'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -79,13 +79,14 @@ pygments_style = 'sphinx'
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
-
 # -- Options for HTML output ----------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -98,12 +99,10 @@ html_theme = 'alabaster'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'MOSFiTdoc'
-
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -133,16 +132,11 @@ latex_documents = [
      'James Guillochon \\& Matt Nicholl', 'manual'),
 ]
 
-
 # -- Options for manual page output ---------------------------------------
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'mosfit', 'MOSFiT Documentation',
-     [author], 1)
-]
-
+man_pages = [(master_doc, 'mosfit', 'MOSFiT Documentation', [author], 1)]
 
 # -- Options for Texinfo output -------------------------------------------
 
@@ -150,10 +144,6 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'MOSFiT', 'MOSFiT Documentation',
-     author, 'MOSFiT', 'One line description of project.',
-     'Miscellaneous'),
+    (master_doc, 'MOSFiT', 'MOSFiT Documentation', author, 'MOSFiT',
+     'One line description of project.', 'Miscellaneous'),
 ]
-
-
-
