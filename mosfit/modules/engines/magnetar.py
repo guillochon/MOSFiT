df81ee15a2cb9ea22aa391d10c2dace77e53ecf9
Matt Nicholl <matt@dhcp-131-142-152-146.cfa.harvard.edu>
2017-03-02 11:57:25 -0500

    Definition of L now matches Ostriker and Gunn 1971. Added some explanatory notes

--- mosfit/modules/engines/magnetar.py
+++ mosfit/modules/engines/magnetar.py
@@ -23,9 +23,13 @@ class Magnetar(Engine):
         self._rest_t_explosion = kwargs['resttexplosion']
 
         Ep = 2.6e52 * (self._Mns / 1.4)**(3. / 2.) * self._Pspin**(-2)
+        # ^ E_rot = 1/2 I (2pi/P)^2, unit = erg
 
         tp = 1.3e5 * self._Bfield**(-2) * self._Pspin**2 * (self._Mns / 1.4)**(
             3. / 2.) * (np.sin(self._thetaPB))**(-2)
+        # ^ tau_spindown = P/(dP/dt), unit = s
+        # Magnetic dipole: power = 2/(3c^3)*(R^3 Bsin(theta))^2 * (2pi/P)^4
+        # Set equal to -d/dt(E_rot) to derive tau
 
         ts = [
             np.inf
@@ -33,12 +37,10 @@ class Magnetar(Engine):
             for x in self._times
         ]
 
-        # print(ts)
-        #
-        # raise SystemExit
-        #
-        luminosities = [Ep / tp / (1. + t * DAY_CGS / tp)**2 for t in ts]
+
+        luminosities = [2 * Ep / tp / (1. + 2 * t * DAY_CGS / tp)**2 for t in ts]
         luminosities = [0.0 if isnan(x) else x for x in luminosities]
+        # ^ From Ostriker and Gunn 1971 eq 4
 
         # Add on to any existing luminosity
         luminosities = self.add_to_existing_lums(luminosities)
