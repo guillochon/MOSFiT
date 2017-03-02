664774da259bcdbc279f74bf98c2459babd4f675
guillochon <guillochon@gmail.com>
2017-03-02 08:51:30 -0500

    MAINT: trying to fix docs

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,9 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.append(os.path.abspath('.'))
+sys.path.append(os.path.abspath('..'))
+sys.path.append(os.path.join(os.path.dirname(__name__), '..'))
 
 from mosfit import __version__
 import sphinx_rtd_theme
