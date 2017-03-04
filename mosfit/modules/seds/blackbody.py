Fixing duplicate code in radio calculation

mosfit/modules/seds/blackbody.py
@@ -8,6 +8,8 @@ from astropy import units as u
 from mosfit.constants import DAY_CGS, FOUR_PI, KM_CGS, M_SUN_CGS
 from mosfit.modules.seds.sed import SED
 
+import matplotlib.pyplot as plt
+
 CLASS_NAME = 'Blackbody'
 
 
@@ -19,8 +21,6 @@ class Blackbody(SED):
     FLUX_CONST = FOUR_PI * (2.0 * c.h * c.c**2 * pi).cgs.value * u.Angstrom.cgs.scale
     X_CONST = (c.h * c.c / c.k_B).cgs.value
     STEF_CONST = (4.0 * pi * c.sigma_sb).cgs.value
-    FLUX_CONST_FREQ = FOUR_PI * (2.0 * c.h / (c.c**2) * pi).cgs.value
-    X_CONST_FREQ = (c.h / c.k_B).cgs.value
 
     def process(self, **kwargs):
         self._luminosities = kwargs['luminosities']
@@ -31,8 +31,6 @@ class Blackbody(SED):
         self._temperature_phot = kwargs['temperaturephot']
         xc = self.X_CONST
         fc = self.FLUX_CONST
-        xcv = self.X_CONST_FREQ
-        fcv = self.FLUX_CONST_FREQ
         cc = self.C_CONST
         temperature_phot = self._temperature_phot
         zp1 = 1.0 + kwargs['redshift']
@@ -51,31 +49,24 @@ class Blackbody(SED):
             if bi >= 0:
                 rest_wavs = (self._sample_wavelengths[bi]
                             * u.Angstrom.cgs.scale / zp1)
-                if not evaled:
-                    sed = ne.evaluate(
-                        'fc * radius_phot**2 / rest_wavs**5 / '
-                        '(exp(xc / rest_wavs / temperature_phot) - 1.0)')
-                    evaled = True
-                else:
-                    sed = ne.re_evaluate()
-
-                sed = np.nan_to_num(sed)
-
-                seds.append(sed)
-
             else:
-                rest_freqs = [self._frequencies[li] * zp1]
-                if not evaled:
-                    sed = ne.evaluate(
-                        'fcv * radius_phot**2 * rest_freqs**3 / '
-                        '(exp(xcv * rest_freqs / temperature_phot) - 1.0)')
-                    evaled = True
-                else:
-                    sed = ne.re_evaluate()
+                rest_wavs = [cc / (self._frequencies[li] * zp1)]
 
-                sed = np.nan_to_num(sed)
+            if not evaled:
+                sed = ne.evaluate(
+                    'fc * radius_phot**2 / rest_wavs**5 / '
+                    '(exp(xc / rest_wavs / temperature_phot) - 1.0)')
+                evaled = True
+            else:
+                sed = ne.re_evaluate()
 
-                seds.append(sed)
+            sed = np.nan_to_num(sed)
+
+            if bi < 0:
+                sed *= rest_wavs**2 / cc
+            # if radio, convert to F_nu
+
+            seds.append(sed)
 
         seds = self.add_to_existing_seds(seds, **kwargs)
 