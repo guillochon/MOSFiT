6120abaab259183896db071ecc382a2db4fe5b5b
Matt Nicholl <matt@dhcp-131-142-152-254.cfa.harvard.edu>
2017-03-13 11:35:06 -0400

    Tidy up magnetar constraints module

--- mosfit/modules/constraints/magnetar_constraints.py
+++ mosfit/modules/constraints/magnetar_constraints.py
@@ -1,6 +1,5 @@
 """Definitions for the `MagnetarConstraints` class."""
 import numpy as np
-from scipy import interpolate
 
 from mosfit.constants import KM_CGS, LIKELIHOOD_FLOOR, M_SUN_CGS, DAY_CGS
 from mosfit.modules.constraints.constraint import Constraint
@@ -17,7 +16,6 @@ class MagnetarConstraints(Constraint):
     def process(self, **kwargs):
         """Process module. Add constraints below."""
         self._score_modifier = 0.0
-
         self._Pspin = kwargs['Pspin']
         self._Mns = kwargs['Mns']
         self._mejecta = kwargs['mejecta'] * M_SUN_CGS
@@ -27,29 +25,28 @@ class MagnetarConstraints(Constraint):
         self._lums = kwargs['luminosities']
         self._redshift = kwargs['redshift']
 
+        # Magnetar rotational energy
         self._Ep = 2.6e52 * (self._Mns / 1.4) ** (3. / 2.) * self._Pspin ** (-2)
 
-        # print('Ep',self._Pspin,self._Mns,self._Ep)
-
+        # Ejecta kinetic energy
         self._Ek = 0.5 * self._mejecta * self._vejecta**2
 
-        # print('Ek',self._mejecta/M_SUN_CGS,self._vejecta/KM_CGS,self._Ek)
-
-
+        # Construct array of rest-frame times since explosion
         norm_times = (self._times - self._t_explosion) / (1.0 + self._redshift)
 
+        # Shift array to get delta_t between observations
         shift_times = norm_times[:-1]
-        shift_times = np.insert(shift_times,0,0.)
+        shift_times = np.insert(shift_times,0,0.0)
 
-        norm_times[norm_times<0] = 0
-        shift_times[shift_times<0] = 0
+        norm_times[norm_times<0] = 0.0
+        shift_times[shift_times<0] = 0.0
 
         L_arr = np.array(self._lums)
 
-        # integrate bolometric light curve
+        # integrate bolometric light curve to find radiative losses
         E_rad = sum(L_arr*(norm_times-shift_times)*DAY_CGS)
 
-        # Kinetic energy < magnetar energy - radiative losses + neutrinos
+        # Kinetic energy < magnetar energy - radiative loss + neutrinos (10^51)
         if (self._Ek > self._Ep - E_rad + 1.e51):
             self._score_modifier += LIKELIHOOD_FLOOR
 
