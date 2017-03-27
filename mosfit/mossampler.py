c89529e26de8ff4550dd31c0777642ec985be767
guillochon <guillochon@gmail.com>
2017-03-27 13:56:01 -0400

    ENH: run acor on each chain

--- mosfit/mossampler.py
+++ mosfit/mossampler.py
@@ -1,13 +1,14 @@
 """Overridden PTSampler with random Gibbs selection."""
-import emcee
 import numpy as np
+from emcee.autocorr import AutocorrError, function
 from emcee.ptsampler import PTLikePrior, PTSampler
 
 
 class MOSSampler(PTSampler):
     """Override PTSampler methods."""
 
-    def get_autocorr_time(self, min_step=0, chain=[], **kwargs):
+    def get_autocorr_time(
+            self, min_step=0, max_walkers=20, chain=[], **kwargs):
         """Return a matrix of autocorrelation lengths.
 
         Returns a matrix of autocorrelation lengths for each
@@ -17,12 +18,89 @@ class MOSSampler(PTSampler):
         acors = np.zeros((self.ntemps, self.dim))
 
         for i in range(self.ntemps):
+            acors[i, :] = 0.0
             if len(chain):
-                x = np.mean(chain[i, :, min_step:, :], axis=0)
+                x = chain[i, :max_walkers, min_step:, :]
             else:
-                x = np.mean(self._chain[i, :, min_step:, :], axis=0)
-            acors[i, :] = emcee.autocorr.integrated_time(x, **kwargs)
+                x = self._chain[i, :max_walkers, min_step:, :]
+            for w in x:
+                acors[i, :] += self.integrated_time(w, **kwargs)
+            acors[i, :] /= len(x)
         return acors
+
+    def integrated_time(
+        self, x, low=10, high=None, step=1, c=5, full_output=False,
+            axis=0, fast=False):
+        """Estimate the integrated autocorrelation time of a time series.
+
+        This estimate uses the iterative procedure described on page 16 of
+        `Sokal's notes <http://www.stat.unc.edu/faculty/cji/Sokal.pdf>`_ to
+        determine a reasonable window size.
+        Args:
+            x: The time series. If multidimensional, set the time axis using
+                the ``axis`` keyword argument and the function will be
+                computed for every other axis.
+            low (Optional[int]): The minimum window size to test. (default:
+                ``10``)
+            high (Optional[int]): The maximum window size to test. (default:
+                ``x.shape[axis] / (2*c)``)
+            step (Optional[int]): The step size for the window search.
+                (default: ``1``)
+            c (Optional[float]): The minimum number of autocorrelation times
+                needed to trust the estimate. (default: ``10``)
+            full_output (Optional[bool]): Return the final window size as well
+                as the autocorrelation time. (default: ``False``)
+            axis (Optional[int]): The time axis of ``x``. Assumed to be the
+                first axis if not specified.
+            fast (Optional[bool]): If ``True``, only use the first ``2^n`` (for
+                the largest power) entries for efficiency. (default: False)
+        Returns:
+            float or array: An estimate of the integrated autocorrelation time
+                of the time series ``x`` computed along the axis ``axis``.
+            Optional[int]: The final window size that was used. Only returned
+                if ``full_output`` is ``True``.
+        Raises
+            AutocorrError: If the autocorrelation time can't be reliably
+                estimated from the chain. This normally means that the chain
+                is too short.
+        """
+        size = 0.5 * x.shape[axis]
+        if int(c * low) >= size:
+            raise AutocorrError("The chain is too short")
+
+        # Compute the autocorrelation function.
+        f = function(x, axis=axis, fast=fast)
+
+        # Check the dimensions of the array.
+        oned = len(f.shape) == 1
+        m = [slice(None), ] * len(f.shape)
+
+        # Loop over proposed window sizes until convergence is reached.
+        if high is None:
+            high = int(size / c)
+        for M in np.arange(low, high, step).astype(int):
+            # Compute the autocorrelation time with the given window.
+            if oned:
+                # Special case 1D for simplicity.
+                tau = 1 + 2 * np.sum(f[1:M])
+            else:
+                # N-dimensional case.
+                m[axis] = slice(1, M)
+                tau = 1 + 2 * np.sum(f[m], axis=axis)
+
+            # Accept the window size if it satisfies the convergence criterion.
+            if np.all(tau > 1.0) and M > c * tau.max():
+                if full_output:
+                    return tau, M
+                return tau
+
+            # If the autocorrelation time is too long to be estimated reliably
+            # from the chain, it should fail.
+            if c * tau.max() >= size:
+                break
+
+        raise AutocorrError("The chain is too short to reliably estimate "
+                            "the autocorrelation time")
 
     def sample(
         self, p0, lnprob0=None, lnlike0=None, iterations=1,
