a2adc2b151905512433af388df595c1690a9df10
guillochon <guillochon@gmail.com>
2017-03-29 15:25:32 -0400

    BUG: `set_attributes` shouldn't be defined

--- mosfit/modules/outputs/output.py
+++ mosfit/modules/outputs/output.py
@@ -7,11 +7,6 @@ from mosfit.modules.module import Module
 class Output(Module):
     """Template class for output Modules."""
 
-    def set_attributes(self, task):
-        """Set module attributes based on task specification."""
-        super(Output, self).set_attributes(task)
-        self._output_keys = task.get('output_keys', [])
-
     def process(self, **kwargs):
         """Process module."""
         return {}
