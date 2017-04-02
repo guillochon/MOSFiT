1364a301672b7b42c4e3ff27af7d58337124083c
guillochon <guillochon@gmail.com>
2017-04-02 17:05:03 -0400

    ENH: greatly sped up covariance likelihood, most time was actually matrix construction.

--- mosfit/__init__.py
+++ mosfit/__init__.py
@@ -6,6 +6,6 @@ from . import plotting
 from . import printer
 from . import utils
 
-__version__ = '0.4.2'
+__version__ = '0.4.3'
 __author__ = 'James Guillochon & Matt Nicholl'
 __license__ = 'MIT'
