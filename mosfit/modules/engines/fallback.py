374cfe9ff6ff5e7fd4005b4c790c0f374fd50e41
Brenna Mockler <bmockler@ucsc.edu>
2017-04-07 14:40:16 -0700

    accidentally committed with testing set to on, now set to off

--- mosfit/modules/engines/fallback.py
+++ mosfit/modules/engines/fallback.py
@@ -38,7 +38,7 @@ class Fallback(Engine):
         Mstarbase = Msolar
         Rstarbase = Rsolar
 
-        self.TESTING = True
+        self.TESTING = False
         ##### FOR TESTING ######
         if self.TESTING == True:
             self.testnum = 0
