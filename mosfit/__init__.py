604dd427811f2dbdcd499edd122c9d2a8bc74862
guillochon <guillochon@gmail.com>
2017-04-08 12:24:02 -0400

    MAINT: updating MANIFEST.in to exclude some cruft

--- mosfit/__init__.py
+++ mosfit/__init__.py
@@ -6,6 +6,6 @@ from . import plotting
 from . import printer
 from . import utils
 
-__version__ = '0.4.7'
+__version__ = '0.4.8'
 __author__ = 'James Guillochon & Matt Nicholl'
 __license__ = 'MIT'
