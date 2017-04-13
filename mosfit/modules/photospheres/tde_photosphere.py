ca691266758661d2f7705ea522ceff3c4fd58208
Brenna Mockler <bmockler@ucsc.edu>
2017-04-13 09:33:52 -0700

    ENH: added soft cuts for min and max photosphere size in tde_photosphere.py

--- mosfit/modules/photospheres/tde_photosphere.py
+++ mosfit/modules/photospheres/tde_photosphere.py
@@ -71,19 +71,22 @@ class tde_photosphere(Photosphere):
             # the luminosity is zero)
 
 
-            rphot2 = (self._Rph_0 * a_p * self._luminosities[ilumzero:]/ Ledd)**self._l
+            # adding rphotmin on to rphot so that there's a soft minimum
+            # also creating soft max by doing inverse( 1/rphot + 1/rphotmax)
+            rphot2 =  (self._Rph_0 * a_p * self._luminosities[ilumzero:]/ Ledd)**self._l 
+            rphot2 = (rphot2 * rphotmax)/(rphot2 + rphotmax) + rphotmin
         
             #print ('rphotmin =', rphotmin, 'rphotmax = ', rphotmax, 'min(rphot2) = ', min(rphot2), 'max(rphot2) = ', max(rphot2) )           
-            rphot2 = np.where(rphot2 > rphotmax, rphotmax, rphot2)
+            #rphot2 = np.where(rphot2 > rphotmax, rphotmax, rphot2)
             # if rphot < rphotmin, set rphot to rphotmin + rphot (makes still dep. on parameters, should
             # help with fitting)   
-            if min(rphot2) < 0: print (rphot2)
-            rphot2 = np.where(rphot2 < rphotmin, rphot2 + rphotmin, rphot2) 
-            if len(rphot2[rphot2 < rphotmin]) > 0 : 
-                print (self._times[ilumzero:][rphot2 < rphotmin])
+            #if min(rphot2) < 0: print (rphot2)
+            #rphot2 = np.where(rphot2 < rphotmin, rphot2 + rphotmin, rphot2) 
+            #if len(rphot2[rphot2 < rphotmin]) > 0 : 
+            #    print (self._times[ilumzero:][rphot2 < rphotmin])
 
             
-            if len(rphotmax[rphotmax<rphotmin])>1 : print ('rphotmin > rphotmax at some point', rphotmax[rphotmax<rphotmin] )
+            #if len(rphotmax[rphotmax<rphotmin])>1 : print ('rphotmin > rphotmax at some point', rphotmax[rphotmax<rphotmin] )
             rphot = np.concatenate((rphot1, rphot2))
             #print (rphotmin, min(rphot), rphot[0])
 
