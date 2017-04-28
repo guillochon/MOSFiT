0389cede865f224c3705289ef145518b1f6fbb78
guillochon <guillochon@gmail.com>
2017-04-27 23:13:26 -0400

    MAINT: more test adjustments

--- test.sh
+++ test.sh
@@ -13,13 +13,13 @@ mpirun -np 2 $RUNNER -m mosfit -e SN2009do --test -i 1 -f 1 -p 0 -F covariance
 $RUNNER -m mosfit -e SN2009do.json --test -i 1 --no-fracking -m magnetar -T 2 -F covariance
 $RUNNER -m mosfit -e mosfit/tests/LSQ12dlf.json --test -i 3 --no-fracking -m csm -F n 6.0 -W 120 -M 0.2 --offline
 $RUNNER -m mosfit -e SN2008ar --test -i 1 --no-fracking -m ia -F covariance
-$RUNNER -m mosfit -e LSQ12dlf --test -i 2 --no-fracking -m rprocess --variance-for-each band --offline
+$RUNNER -m mosfit -e mosfit/tests/LSQ12dlf.json --test -i 2 --no-fracking -m rprocess --variance-for-each band --offline
 if [ "$1" = -c ]; then
     $RUNNER -m mosfit -e SN2007bg --test -i 1 --no-fracking -m ic --language ru
 else
     $RUNNER -m mosfit -e SN2007bg --test -i 1 --no-fracking -m ic
 fi
-$RUNNER -m mosfit -e 12dlf --test -i 1 --no-fracking -m slsn -S 20 -E 10.0 100.0 -g -c --no-copy-at-launch -x radiusphot
+$RUNNER -m mosfit -e 09do --test -i 1 --no-fracking -m slsn -S 20 -E 10.0 100.0 -g -c --no-copy-at-launch -x radiusphot
 $RUNNER -m mosfit -e mosfit/tests/SN2006le.json --test -i 5 --no-fracking -m csmni --extra-bands u g --extra-instruments LSST -L 55540 55560 --exclude-bands B -s test --quiet -u --offline
 
 if [ "$1" = -c ]; then
