9412b4e7141037a7e032f1df8cfb152fca6b8cc2
guillochon <guillochon@gmail.com>
2017-05-08 21:43:47 -0400

    MAINT: bump version

--- mosfit/__init__.py
+++ mosfit/__init__.py
@@ -6,6 +6,6 @@ from . import plotting  # noqa: F401
 from . import printer  # noqa: F401
 from . import utils  # noqa: F401
 
-__version__ = '0.5.12'
+__version__ = '0.6.0'
 __author__ = 'James Guillochon & Matt Nicholl'
 __license__ = 'MIT'
