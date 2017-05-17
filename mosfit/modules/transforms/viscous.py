90610369f09cae02903e8eff7bd8bf0c23438ecb
Brenna Mockler <bmockler@ucsc.edu>
2017-05-17 09:41:21 -0700

    ENH: added viscous delay back in to tde module

--- mosfit/modules/transforms/viscous.py
+++ mosfit/modules/transforms/viscous.py
@@ -10,10 +10,9 @@ CLASS_NAME = 'Viscous'
 class Viscous(Transform):
     """Viscous delay transform."""
 
-    logsteps = True
-    N_INT_TIMES = 1000
-    testnum = 0
-    TESTING = False
+    logsteps = False
+    N_INT_TIMES = 1000  # 1e5
+
     def process(self, **kwargs):
         """Process module."""
         super(Viscous, self).process(**kwargs)
@@ -21,16 +20,24 @@ class Viscous(Transform):
         ipeak = np.argmax(self._dense_luminosities)
         tpeak = self._dense_times_since_exp[ipeak]
         Tvisc = kwargs['Tviscous'] * tpeak
+        # if kwargs['Tviscous'] > 1.0:
+        #     self.N_INT_TIMES = 1000
+        #     dense_frac = 0.2
+        if kwargs['Tviscous'] <= 1:
+            extra_steps = int(1000. - 1000*np.log10(kwargs['Tviscous']))
+        else:
+            extra_steps = int(1000./(1 + np.log10(kwargs['Tviscous'])))
 
         new_lum = []
         evaled = False
         lum_cache = {}
         lum_func = interp1d(self._dense_times_since_exp,
-                            self._dense_luminosities)
-        timesteps = self.N_INT_TIMES
+                            self._dense_luminosities,
+                            copy=False, assume_sorted=True)
         min_te = min(self._dense_times_since_exp)
+        max_te = max(self._dense_times_since_exp)
 
-        for j, te in enumerate(self._times_to_process):
+        for j, te in enumerate(self._dense_times_since_exp):
             if te <= 0.0:
                 new_lum.append(0.0)
                 continue
@@ -51,12 +58,32 @@ class Viscous(Transform):
                 if int_times[-1] > te:
                     int_times[-1] = te
             else:
-                int_times = np.linspace(min_te, te, timesteps)
+                base_times = np.linspace(min_te, te, self.N_INT_TIMES)
+                if (te - min_te) < 0.01*(max_te - min_te):
+                    int_times = np.unique(np.concatenate(
+                        (np.linspace(min_te, te, extra_steps), base_times)))
+                else:
+                    tearly = min_te + 0.005*(max_te - min_te)
+                    tm = te - 0.005*(max_te - min_te)
+                    int_times = np.unique(np.concatenate(
+                        (np.linspace(min_te, tearly, extra_steps), base_times,
+                            np.linspace(tm, te, extra_steps))))
+
+                # if te - min_te > 0.005 * (max_te - min_te):
+                #    tm = te - 0.005 * (max_te - min_te)
+                #    num_sparse = int(self.N_INT_TIMES*0.2)
+                #    sparse_times = np.linspace(min_te, tm, num_sparse)
+                #    times_nearte = np.linspace(tm, te, self.N_INT_TIMES -
+                #                               num_sparse)
+                #    int_times = np.concatenate((sparse_times, times_nearte))
+                # else:
+                #    int_times = np.linspace(min_te, te, self.N_INT_TIMES)
                 # all the spacings are the same bc of linspace
-                dt = int_times[1] - int_times[0]
+                # dt = int_times[1] - int_times[0]
 
             int_lums = lum_func(int_times)
 
+            # int_arg = np.exp((-te + int_times)/Tvisc) * int_lums
             if not evaled:
                 int_arg = ne.evaluate(
                     'exp((-te + int_times)/Tvisc) * int_lums')
@@ -71,17 +98,20 @@ class Viscous(Transform):
             if self.logsteps:
                 lum_val = np.trapz(int_arg, int_times) / Tvisc
             else:
-                lum_val = np.trapz(int_arg, dx=dt) / Tvisc
+                # STOPPED HERE
+                lum_val = np.trapz(int_arg, int_times) / Tvisc
+                #lum_val = np.trapz(int_arg, dx=dt) / Tvisc
+                #lum_val = (np.sum(int_arg[1:-1]) +
+                #           0.5 * (int_arg[0] + int_arg[-1]))/Tvisc
+                #lum_val *= (int_times[-1] -
+                #            int_times[0])/(2.0 * (len(int_times) - 1))
 
             lum_cache[te] = lum_val
             new_lum.append(lum_val)
 
-        # ----------------TESTING ----------------
-        if self.TESTING == True:
-            np.savetxt('test_dir/test_viscous/endviscous/time+dmdt'+'{:03d}'.format(self.testnum)+'.txt',
-                        (self._times_to_process, new_lum)) # set time = 0 when explosion goes off
-            self.testnum += 1
-    
-        # ----------------------------------------
-
-        return {'dense_luminosities': new_lum}
+            previscous_lums = self._dense_luminosities
+            postviscous_lums = new_lum
+        return {'dense_luminosities': new_lum,
+                'previscous_lums': previscous_lums,
+                'postviscous_lums': postviscous_lums,
+                'viscous_times': self._dense_times_since_exp}
