c93221442a1a07ed5b72111b3051b09f6b911d15
guillochon <guillochon@gmail.com>
2017-08-24 22:31:37 -0400

    MAINT: change test slightly

--- test.py
+++ test.py
@@ -3,17 +3,18 @@ import mosfit
 import numpy as np
 
 # Test running the fitter.
-my_fitter = mosfit.fitter.Fitter()
+my_fitter = mosfit.fitter.Fitter(quiet=False, test=True, offline=True)
 
 print('Running `fit_events` test.')
 entries, ps, lnprobs = my_fitter.fit_events(
     events=['SN2009do', 'SN2007bg'], models=['magni', 'slsn'], iterations=1,
-    quiet=False, test=True, offline=True, user_fixed_parameters=['covariance'])
+    user_fixed_parameters=['covariance'])
 
 print('Model WAICs: ',
       [[y['models'][0]['score']['value'] for y in x] for x in entries])
 
 # Test a single call to the model.
+print('Testing single call to Model.likelihood().')
 my_fetcher = mosfit.fetcher.Fetcher(my_fitter)
 
 fetched = my_fetcher.fetch('SN2009do')[0]
