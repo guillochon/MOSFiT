b1b14e58aa330e4c5747221dca378aff650db6c6
guillochon <guillochon@gmail.com>
2018-03-12 22:13:06 -0400

    MAINT: merging line fixes

--- mosfit/modules/seds/line.py
+++ mosfit/modules/seds/line.py
@@ -40,7 +40,7 @@ class Line(SED):
         zp1 = 1.0 + kwargs[self.key('redshift')]
         czp1A = cc / (zp1 * u.Angstrom.cgs.scale)
 
-        amps = np.array([
+        amps = self._line_amplitude * np.array([
             np.exp(-0.5 * (
                 (x - self._rest_t_explosion - self._line_time) /
                 self._line_duration) ** 2) for x in self._times]) / (
@@ -51,7 +51,8 @@ class Line(SED):
         if self._seds is None:
             raise ValueError(prt.message('line_sed'))
 
-        seds = [x / (1.0 - amps[xi]) for xi, x in enumerate(self._seds)]
+        seds = [x * (1.0 - amps[xi]) for xi, x in enumerate(self._seds)]
+        amps *= self._luminosities / (ls * SQRT_2_PI)
         amps_dict = {}
         evaled = False
 
