c229d4e076677613cb8a98c2369ceb50de7f481e
mnicholl <mattnicholl1@gmail.com>
2022-04-11 16:25:01 +0100

    Change cocoon implementation: better T, R, now specify shocked fraction directly - easier to treat ejecta components separately if needed

--- mosfit/modules/engines/shock_cocoon.py
+++ mosfit/modules/engines/shock_cocoon.py
@@ -16,7 +16,8 @@ class Shock(Engine):
 
     Follows Piro and Kollmeier 2018.
 
-    Shock heating can be turned off in bns model by setting cos_theta_cocoon=1
+    Shock heating can be turned off in bns model by setting shock_frac=0
+    or cos_theta_cocoon=1
     """
 
     DIFF_CONST = M_SUN_CGS / (FOUR_PI * C_CGS * KM_CGS)
@@ -35,20 +36,23 @@ class Shock(Engine):
         # Cocoon opening angle- should be between 1 and cos_theta_open
         # for polar KN ejecta
         self._cos_theta_cocoon = kwargs[self.key('cos_theta_cocoon')]
+        self._shocked_fraction = kwargs[self.key('shock_frac')]
         # Shocked ejecta power law density profile
         self._s =  kwargs[self.key('s')]
         # Shock breakout timescale in seconds
         self._t_shock =  kwargs[self.key('tshock')]
         # Radius where material is shocked by relativistic jet:
         R = C_CGS * self._t_shock
+        
+        m_shocked = self._m_ejecta * self._shocked_fraction
 
         theta = np.arccos(self._cos_theta_cocoon)
 
         self._tau_diff = np.sqrt(self.DIFF_CONST * self._kappa *
-                                 self._m_ejecta / self._v_ejecta) / DAY_CGS
+                                 m_shocked / self._v_ejecta) / DAY_CGS
 
 
-        L0 = (theta**2/2)**(1/3) * (self._m_ejecta * M_SUN_CGS *
+        L0 = (theta**2/2)**(1/3) * (m_shocked * M_SUN_CGS *
                 self._v_ejecta * KM_CGS * R / (self._tau_diff * DAY_CGS)**2 )
 
         ts = [
